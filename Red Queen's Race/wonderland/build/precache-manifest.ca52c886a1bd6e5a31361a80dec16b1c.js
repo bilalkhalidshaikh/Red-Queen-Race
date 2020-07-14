0f6d933678d490cc5ae7f3a77e8d1ce646fe0e64
bilalkhalidshaikh <muhammadbilalkhalid56@gmail.com>
2020-07-14 20:40:34 +0500

    Red Queen's Race

---
+++ Red Queen's Race/wonderland/build/precache-manifest.ca52c886a1bd6e5a31361a80dec16b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431d53f19b9e18fe9029ffe43af44bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6ffca6c0ab6bc6af74",
+    "url": "/static/css/main.cdf772da.chunk.css"
+  },
+  {
+    "revision": "6cd9dcf8777a52eba4a2",
+    "url": "/static/js/2.118e8e3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.118e8e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6ffca6c0ab6bc6af74",
+    "url": "/static/js/main.76796927.chunk.js"
+  },
+  {
+    "revision": "ab4f07c2760ac7d877a5",
+    "url": "/static/js/runtime-main.008345c2.js"
+  }
+]);
